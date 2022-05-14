bccd4669c061da5b4ab618c53d0a5a6d3ba97b50
andrejs.dasko <andrejs.dasko@gmail.com>
2022-05-14 22:51:19 +0300

    Added command line interface

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,23 @@
 package com.example.demo;
 
+import com.example.demo.cli.Operator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
+
+	@Autowired
+	private Operator operator;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) {
+		operator.connection();
+	}
 }
